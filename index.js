7044769891a97abd48a331d039ee89f8c77eb1a3
Kanha Bhawani <ghanishtsinghal@gmail.com>
2024-02-22 10:40:32 +0530

    deploy server

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/webhook', (req, res) => {
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
 
-  if (mode && token === VERIFY_TOKEN) {
+  if (mode && token === 'abcdefgh') {
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
